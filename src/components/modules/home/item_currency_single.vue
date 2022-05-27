6530b907b7643e26258839cccfb45897f31d6c06
lemonWM <arturchochla@gmail.com>
2022-05-27 17:01:47 +0200

    single + chart.js

---
+++ src/components/modules/home/item_currency_single.vue
@@ -0,0 +1,191 @@
+<template>
+    <div class="single-currency-wrapper">
+        <div class="single-currency-wrapper__header">
+            <a-card title="" style="">
+                <p>{{ state.currency.name }} <span>{{ state.currency.symbol }}</span></p>
+                <p><span>Current price: </span>{{ state.currency.priceUsd }} <span>USD</span></p>
+            </a-card>
+        </div>
+        <div class="single-currency-wrapper__bar">
+            <a-radio-group v-model:value="state.period" @change="selectPeriod">
+                <a-radio-button value="y" loading>Years</a-radio-button>
+                <a-radio-button value="d">Days</a-radio-button>
+                <a-radio-button value="h">Hours</a-radio-button>
+            </a-radio-group>
+        </div>
+        <div class="single-currency-wrapper__details">
+
+            <div v-if="state.period == 'y'" class="canva_1">
+                <canvas id="myChart" width="80vw" height="300"></canvas>
+            </div>
+            <div v-else-if="state.period == 'd'" class="canva_2">
+                <canvas id="myChart2" width="80vw" height="300"></canvas>
+            </div>
+            <div v-else-if="state.period == 'h'" class="canva_3">
+                <canvas id="myChart3" width="80vw" height="300"></canvas>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { reactive, onMounted } from 'vue'
+    import { useRoute } from 'vue-router'
+    import axios from 'axios'
+    import Chart from 'chart.js/auto';
+    import zoomPlugin from 'chartjs-plugin-zoom'
+
+
+    Chart.register(zoomPlugin)
+
+    export default {
+        name: 'currency-single',
+
+        setup() {
+            const route = useRoute()
+            
+            const state = reactive({
+                period: 'y',
+                currency: {},
+                currencyDetails: [],
+                currencyValues: [],
+                currencyDates: []
+            })
+            const currencySingle = route.params.id
+            
+            let myChart = null
+
+            const selectPeriod = function(){
+                
+                if(state.period == 'y'){
+
+                    getCurrentDetails()
+                }
+                else if(state.period == 'd') {
+                    
+                }
+                else {
+                    
+                }
+
+            }
+
+            const getCurrent = function(){
+
+                axios.get(`https://api.coincap.io/v2/assets/${currencySingle}`)
+                .then(response => {  
+                    
+                    state.currency = response.data.data
+                    state.currency.priceUsd = parseInt(state.currency.priceUsd)
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+            }
+
+            const getCurrentDetails = function(){
+                
+                axios.get(`https://api.coincap.io/v2/assets/${currencySingle}/history?interval=d1`)
+                .then(response => {  
+                    
+                    let arr = []
+
+                    arr = response.data.data
+
+                    state.currencyDetails = arr
+
+                    chartData();
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+            }
+
+            const chartData = function() {
+
+                state.currencyValues = []
+                state.currencyDates = []
+
+                state.currencyDetails.forEach((x, i) => {
+                    let value = parseInt(x.priceUsd),
+                        miliseconds = x.time,
+                        dateObj = new Date(miliseconds),
+                        month = dateObj.getUTCMonth() + 1,
+                        day = dateObj.getUTCDate(),
+                        year = dateObj.getUTCFullYear(),
+                        fullDate = year + "/" + month + "/" + day;
+
+                    state.currencyValues.push(value)
+                    state.currencyDates.push(fullDate)                    
+                });
+         
+                if (myChart != null) {
+                    myChart.destroy()
+                }
+                const ctx = document.getElementById('myChart');
+
+                myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: state.currencyDates,
+                    datasets: [{
+                        label: '',
+                        data: state.currencyValues,
+                        backgroundColor: ['rgba(25, 31, 56,1)'],
+                        borderColor: ['rgba(25, 31, 56,1)'],
+                        borderWidth: 2,
+                        tension: .5
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false
+                        },
+                        x: {
+                            beginAtZero: true
+                        },
+                    },
+                    plugins: {
+                        zoom: {
+                            zoom: {
+                                wheel: {
+                                    enabled: true,
+                                },
+                                pinch: {
+                                    enabled: true
+                                },
+                                mode: 'xy',
+                                },
+                            animation: {
+                                duration: 1000,
+                                easing: 'easeOutCubic'
+                            },
+                            limits: {
+                                x: {min: 0,  minRange: 50},
+                                y: {min: 0,  minRange: 50}
+                            },
+                        }
+                    },
+                    layout: {
+                        padding: 0
+                    }
+                }
+            });  
+        } 
+            
+         onMounted(() => {
+                
+            getCurrent();
+            getCurrentDetails();
+                
+        });
+
+        return {
+            state, selectPeriod
+        }
+    }
+} 
+</script>
--- src/views/crypto-single.vue
+++ src/views/crypto-single.vue
@@ -1,46 +1,7 @@
 <template>
-    <div class="single-currency-wrapper">
-        <a-card title="" style="">
-            <p>{{ state.currency.name }} <span>{{ state.currency.symbol }}</span></p>
-        </a-card>
-    </div>
+  <singleCurrency />
 </template>
 
-<script>
-    import { reactive, onMounted } from 'vue'
-    import { useRoute } from 'vue-router'
-    import axios from 'axios'
-
-    export default {
-        props: ['id', 'name'],
-        name: 'currency-single',
-
-        setup() {
-            const route = useRoute()
-            
-            const state = reactive({
-                currency: {}
-            })
-            const currencySingle = route.params.id
-            
-            onMounted(() => {
-                
-                axios.get(`https://api.coincap.io/v2/assets/${currencySingle}`)
-                .then(response => {  
-                    
-                    state.currency = response.data.data
-
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-            });
-
-            return {
-                state
-            }
-        }
-
-    }
-
+<script setup>
+    import singleCurrency from '../components/modules/home/item_currency_single.vue'
 </script>
